95c313cd16c7d8dbf29418a502c1505af150f295
d36choi <yuio1126@gmail.com>
2023-12-25 22:45:27 +0900

    feat: 1등 랭킹 page api 구현

--- database/shop_crud.py
+++ database/shop_crud.py
@@ -1,7 +1,10 @@
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from database import models
+from database.models.shop import WinningShop
 
 
-def get_shop(db: Session, offset: int = 0, limit: int = 10):
-    return db.query(models.Shop).offset(offset).limit(limit).all()
+def get_shop_rankings(db: Session, offset: int = 0, limit: int = 10):
+    # button 구현이 필요한 경우: https://uriyyo-fastapi-pagination.netlify.app/
+    return db.query(WinningShop).order_by(WinningShop.count_shop_first.desc()).offset(offset * limit).limit(limit).all()
--- routes/shop.py
+++ routes/shop.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter, Depends
 
 from sqlalchemy.orm import Session
 
+from database.shop_crud import get_shop_rankings
 from model.shop import ShopInfoDto
-from database.models.shop import ShopInfo
+from database.models.shop import ShopInfo, WinningShop
 from database import SessionLocal
+from model.shop_winning_model import WinningShop
 
 shop_router = APIRouter(
     tags=["Shop"]
@@ -19,9 +21,9 @@ def get_db():
         db.close()
 
 
-# @shop_router.get("/shop")
-# async def read_shop(db: Session = Depends(get_db), page: int = 0, size: int = 10) -> list[ShopBase]:
-#     return shop_crud.get_shop(db, page, size)
+@shop_router.get("/ranking")
+async def read_shop(db: Session = Depends(get_db), page: int = 0, size: int = 10) -> list[WinningShop]:
+    return get_shop_rankings(db, page, size)
 
 @shop_router.get("/shop-info")
 async def read_shop_info(db: Session = Depends(get_db)) -> list[ShopInfoDto]:
