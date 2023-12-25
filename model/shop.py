76e5ddaeef7240fff173b980fddbab72619e1881
d36choi <yuio1126@gmail.com>
2023-12-25 22:44:06 +0900

    remove: 안쓰게 된 테이블 dto 제거

--- model/shop.py
+++ model/shop.py
@@ -1,25 +1,7 @@
 from pydantic import BaseModel
 
-
-class ShopBase(BaseModel):
-    name: str
-    location: str
-    firstPrizeCount: int | None = None
-    secondPrizeCount: int | None = None
-
 class ShopInfoDto(BaseModel):
     retailer_id: int
     address: str
     name: str
     phone_number: str
-
-
-class ShopCreate(ShopBase):
-    pass
-
-
-class Shop(ShopBase):
-    id: int
-
-    class Config:
-        orm_mode = True
